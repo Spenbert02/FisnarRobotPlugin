1f1efa41517c1255467f91165f81f8d44e617191
lab_computer <spenbert02@gmail.com>
2022-05-25 12:14:09 -0500

    fisnar rs232 testing

--- tests/serialTesting.py
+++ tests/serialTesting.py
@@ -30,26 +30,48 @@ product: None  (NoneType)
 interface: None  (NoneType)
 """
 
+"""
+Fisnar RS 232 Protocol Settings (from fisnar f5200n.1 manual, 'remote command' section)
+
+baud rate: 115200
+# of bits: 8
+parity: none
+stop bits: 1
+"""
+
 
 # # 5/24 tests (trying to interface with Nordson pressure controller from Python)
+# import serial
+#
+# # creating serial port object
+# pressureController = serial.Serial("COM7", timeout=10)  # 10 indicates a read timeout of 10 seconds
+#
+# # preparing string and converting string to ascii bytes
+# str_to_write = f"{chr(5)}{chr(2)}03DI F0{chr(3)}{chr(4)}"  # ascii characters: ENQ, STX, ETX, EOT
+# bytes_to_write = bytes(str_to_write, "ascii")
+#
+# # writing command to dispenser and reading returned line
+# write_ret_val = pressureController.write(bytes_to_write)
+# confirm_line = pressureController.read_until(expected=bytes(chr(3), "ascii"))  # reading input buffer until 0x03 (ETX) character is read
+#
+# # printing returned line
+# print(confirm_line)
+
+
+# 5/25 tests - trying to figure out the command format the fisnar takes
 import serial
 
-pressureController = serial.Serial("COM7", timeout=10)  # 10 indicates a read timeout of 10 seconds
+# creating port object
+fisnar = serial.Serial("COM7", 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=10)  # assuming the default communication settings? Not sure where to find this info, wouldn't be surprised if its some weird settings
 
-str_to_write = f"{chr(5)}{chr(2)}03DI F0{chr(3)}{chr(4)}"  # ascii characters: ENQ, STX, ETX, EOT
-bytes_to_write = bytes(str_to_write, "ascii")
+command_1 = bytes.fromhex("f0 f0")
+command_2 = bytes.fromhex("e8")
 
-write_ret_val = pressureController.write(bytes_to_write)
-print(write_ret_val)
-print(pressureController.in_waiting)
-confirm_line = pressureController.readline()
+fisnar.write(bytes_to_write)
+
+confirm_line = fisnar.read()
 
 print(confirm_line)
-
-
-
-
-
 
 
 
