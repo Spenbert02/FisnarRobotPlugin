55801eb3fbb28aefb8829044ae6250d719b8205f
lab_computer <spenbert02@gmail.com>
2022-05-25 17:54:44 -0500

    serial testing

--- tests/serialTesting.py
+++ tests/serialTesting.py
@@ -58,21 +58,62 @@ stop bits: 1
 # print(confirm_line)
 
 
-# 5/25 tests - trying to figure out the command format the fisnar takes
-import serial
+# # function to get check byte from a string of hex bytes
+# def getCheckSumByte(bytes_to_sum):
+#     tally = 0
+#     for b in bytes_to_sum:
+#         tally += b
+#
+#     return bytes.fromhex(hex(tally % 256)[2:])
+#
+#
+# sample = bytes.fromhex("01 00 00 00 20 41 00 00 20 41 00 00 20 41 00 00 00 1f")
+# print(getCheckSumByte(sample))
+#
+# print(sample + )
 
-# creating port object
-fisnar = serial.Serial("COM7", 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=10)  # assuming the default communication settings? Not sure where to find this info, wouldn't be surprised if its some weird settings
 
-command_1 = bytes.fromhex("f0 f0")
-command_2 = bytes.fromhex("e8")
 
-fisnar.write(bytes_to_write)
 
-confirm_line = fisnar.read()
 
-print(confirm_line)
+# # 5/25 tests - trying to figure out the command format the fisnar takes
+# import serial
+#
+# # creating port object
+# fisnar = serial.Serial("COM7", 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, timeout=10)  # assuming the default communication settings? Not sure where to find this info, wouldn't be surprised if its some weird settings
+#
+# command_1 = bytes.fromhex("f0 f0")  # wait until f0 recieved afterwards
+# command_2 = bytes.fromhex("e8")  # 'pre-command' command
+# # command_3 = bytes.fromhex("aa 01 00 00 00 20 41 00 00 20 41 00 00 20 41 00 00 00 1f 43 00")  # actual command
+# command_3 = bytes.fromhex("aa 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00")  # test empty
+# command_4 = bytes.fromhex("f1 f1")  # wait until f1 recieved afterwards
+#
+# # writing command so fisnar knows to listen
+# fisnar.write(command_1)
+# print("command 1 (" + str(command_1) + ") sent")
+# command_1_ret = fisnar.read_until(expected=bytes.fromhex("f0"))
+# if command_1_ret == bytes.fromhex("f0"):
+#     print("f0 recieved. proceeding...")
+#
+#     # writing actual command (and xe8 pre command)
+#     fisnar.write(command_2)
+#     print("command 2 (" + str(command_2) + ") sent")
+#     fisnar.write(command_3)
+#     print("command 3 (" + str(command_3) + ") sent")
+#     command_3_ret = fisnar.read_until(expected=bytes.fromhex("01"))
+#     if command_3_ret == bytes.fromhex("01"):
+#         print("checksum recieved, proceeding...")
+#
+#         # writing 'end' command
+#         fisnar.write(command_4)
+#         print("command 4 (" + str(command_4) + ") sent")
+#         command_4_ret = fisnar.read_until(expected="f1")
+#         if command_4_ret == bytes.fromhex("f1"):
+#             print("f1 recieved. all done")
 
+
+if __name__ == "__main__":
+    
 
 
 
