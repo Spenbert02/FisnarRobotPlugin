025b72c26659e0f98df31296370d985fbf04a239
Spencer Bertram <Spenbert02@gmail.com>
2023-04-25 19:15:47 -0500

    fixed zipper

--- tests/zipper.py
+++ tests/zipper.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     with ZipFile(zip_file_path, "w") as zip_obj:
         for top_folder, sub_folders, files in os.walk(content_folder_path):
-            if "virtEnv" not in top_folder and "VirtEnv" not in top_folder and "tests" not in top_folder and "pycache" not in top_folder and ".git" not in top_folder and "plugin_download":  # disregarding virtual environment folder
+            if "virtEnv" not in top_folder and "VirtEnv" not in top_folder and "tests" not in top_folder and "pycache" not in top_folder and ".git" not in top_folder and "plugin_download" not in top_folder:  # disregarding virtual environment folder
                 for filename in files:
                     file_path = os.path.join(top_folder, filename)
                     zip_obj.write(file_path, os.path.join("FisnarRobotPlugin", os.path.relpath(file_path, content_folder_path)))
